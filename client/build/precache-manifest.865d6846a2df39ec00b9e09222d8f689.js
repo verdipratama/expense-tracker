20c706a0444b408d911618cfee37878b9cf20692
verdipratama <verdipratama@icloud.com>
2020-03-04 17:29:38 +0700

    add build

---
+++ client/build/precache-manifest.865d6846a2df39ec00b9e09222d8f689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a95b5ae2e8940ca78d17e9547e4da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9d1f993aaf3ae1bea7",
+    "url": "/static/css/main.a5d66f92.chunk.css"
+  },
+  {
+    "revision": "987bb04c6ea232ea727c",
+    "url": "/static/js/2.5860b85e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5860b85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9d1f993aaf3ae1bea7",
+    "url": "/static/js/main.c0ed3fba.chunk.js"
+  },
+  {
+    "revision": "2807fd1ac2ae16ffa310",
+    "url": "/static/js/runtime-main.97709d72.js"
+  }
+]);
